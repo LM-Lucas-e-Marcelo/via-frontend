31445ff2e362f4b6f941614795465870725ee018
Lucas Sagas <lucas.sagas@rdstation.com>
2025-05-08 12:08:19 -0300

    Update hero component background image from SVG to PNG

--- src/components/hero.tsx
+++ src/components/hero.tsx
@@ -7,7 +7,7 @@ export const Hero = () => {
     window.open("/services/7", "_self");
   };
   return (
-    <div className="w-full bg-[url(/banner-hero.svg)] p-10 pb-0 bg-cover bg-center flex items-center justify-center">
+    <div className="w-full bg-[url(/banner-hero.png)] p-10 pb-0 bg-cover bg-center flex items-center justify-center">
       <div className="w-full max-w-[1440px] min-h-[400px] mt-[74px] flex items-center justify-around flex-col gap-10 md:gap-0 md:flex-row">
         <motion.div
           className="flex flex-col gap-2 sm:gap-4 max-w-[600px]"
