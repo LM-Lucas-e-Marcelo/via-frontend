70d1ee1c222354ebaa6462358a064c1d3256d8e0
Lucas Sagas <lucas.sagas@rdstation.com>
2025-05-14 15:01:38 -0300

    Update Hero component: change text color from white to zinc-900 for improved readability and adjust image width

--- src/components/hero.tsx
+++ src/components/hero.tsx
@@ -15,11 +15,11 @@ export const Hero = () => {
           animate={{ opacity: 1, x: 0 }}
           transition={{ duration: 1 }}
         >
-          <h1 className="sm:text-3xl text-2xl font-bold uppercase text-zinc-900">
+          <h1 className="sm:text-3xl text-2xl font-bold uppercase text-white">
             Antes de investir no carro dos seus sonhos, deixe a VIA analisar
             cada detalhe.
           </h1>
-          <h2 className="text-lg sm:text-xl text-zinc-900">
+          <h2 className="text-lg sm:text-xl text-white">
             Com a nossa análise completa, você faz a escolha certa. A VIA te
             ajuda a enxergar além do óbvio!
           </h2>
@@ -29,7 +29,7 @@ export const Hero = () => {
         </motion.div>
 
         <motion.img
-          className="w-[350px]"
+          className="w-[450px]"
           src={modelCarMobile}
           alt="a white car"
           initial={{ opacity: 0, x: 100 }}
