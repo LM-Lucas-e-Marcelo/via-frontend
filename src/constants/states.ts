f1a41719cd5766dd0cfcc83474b904955d0614dd
Lucas Sagas <lucas.sagas@rdstation.com>
2025-05-22 13:20:16 -0300

    Add constants for Brazilian states

---
+++ src/constants/states.ts
@@ -0,0 +1,110 @@
+export const STATES = [
+  {
+    name: "Acre",
+    value: "Acre",
+  },
+  {
+    name: "Alagoas",
+    value: "Alagoas",
+  },
+  {
+    name: "Amapá",
+    value: "Amapá",
+  },
+  {
+    name: "Amazonas",
+    value: "Amazonas",
+  },
+  {
+    name: "Bahia",
+    value: "Bahia",
+  },
+  {
+    name: "Ceará",
+    value: "Ceará",
+  },
+  {
+    name: "Espírito Santo",
+    value: "Espírito Santo",
+  },
+  {
+    name: "Federal District",
+    value: "Federal District",
+  },
+  {
+    name: "Goiás",
+    value: "Goiás",
+  },
+  {
+    name: "Maranhão",
+    value: "Maranhão",
+  },
+  {
+    name: "Mato Grosso",
+    value: "Mato Grosso",
+  },
+  {
+    name: "Mato Grosso do Sul",
+    value: "Mato Grosso do Sul",
+  },
+  {
+    name: "Minas Gerais",
+    value: "Minas Gerais",
+  },
+  {
+    name: "Pará",
+    value: "Pará",
+  },
+  {
+    name: "Paraíba",
+    value: "Paraíba",
+  },
+  {
+    name: "Paraná",
+    value: "Paraná",
+  },
+  {
+    name: "Pernambuco",
+    value: "Pernambuco",
+  },
+  {
+    name: "Piauí",
+    value: "Piauí",
+  },
+  {
+    name: "Rio de Janeiro",
+    value: "Rio de Janeiro",
+  },
+  {
+    name: "Rio Grande do Norte",
+    value: "Rio Grande do Norte",
+  },
+  {
+    name: "Rio Grande do Sul",
+    value: "Rio Grande do Sul",
+  },
+  {
+    name: "Rondônia",
+    value: "Rondônia",
+  },
+  {
+    name: "Roraima",
+    value: "Roraima",
+  },
+  {
+    name: "Santa Catarina",
+    value: "Santa Catarina",
+  },
+  {
+    name: "São Paulo",
+    value: "São Paulo",
+  },
+  {
+    name: "Sergipe",
+    value: "Sergipe",
+  },
+  {
+    name: "Tocantins",
+    value: "Tocantins",
+  },
+];
