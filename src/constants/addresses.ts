36c0e2e6ae91f8c54a81ac8d021f061ef8d30f04
Lucas Sagas <lucas.sagas@rdstation.com>
2025-05-23 15:52:31 -0300

    Update phone and WhatsApp numbers in addresses constant

--- src/constants/addresses.ts
+++ src/constants/addresses.ts
@@ -25,8 +25,8 @@ export const addresses = [
     cityAndNeigh: "VIA VISTORIA BIGUAÇU",
     street:
       "R. Cônego Rodolfo Machado, 1000 - Rio Caveiras, Biguaçu - SC - Centro Comercial BIG MIX",
-    phoneNumber: "(48) 99149-4123",
-    whatsappNumber: "48991494123",
+    phoneNumber: "(48) 99192-0452",
+    whatsappNumber: "48991920452",
     mapUrl:
       "https://www.google.com/maps/place/R.+C%C3%B4nego+Rodolfo+Machado,+1000+-+Rio+Caveiras,+Bigua%C3%A7u+-+SC,+88161-732/@-27.5064736,-48.6670961,14.58z/data=!4m6!3m5!1s0x95274c3bc4256687:0xd8aa1ff4adcd8118!8m2!3d-27.5055183!4d-48.6530734!16s%2Fg%2F11gdg5j99v?entry=ttu",
     image: biguacu,
