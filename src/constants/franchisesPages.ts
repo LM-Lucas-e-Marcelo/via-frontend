4f1c39e67cc336a9473df60294daa3a0059f7500
Lucas Sagas <lucas.sagas@rdstation.com>
2025-06-17 12:39:55 -0300

    add franchises pages with WhatsApp and localization details

---
+++ src/constants/franchisesPages.ts
@@ -0,0 +1,63 @@
+import whatsappBiguacu from "../assets/franchises/biguacu/whatsapp.webp";
+import localizationBiguacu from "../assets/franchises/biguacu/localization.webp";
+import whatsappMeleiro from "../assets/franchises/meleiro/whatsapp.webp";
+import localizationMeleiro from "../assets/franchises/meleiro/localization.webp";
+import whatsappPortoUniao from "../assets/franchises/porto-uniao/whatsapp.webp";
+import localizationPortoUniao from "../assets/franchises/porto-uniao/localization.webp";
+import whatsappIcara from "../assets/franchises/icara/whatsapp.webp";
+import localizationIcara from "../assets/franchises/icara/localization.webp";
+import whatsappCriciuma from "../assets/franchises/criciuma/whatsapp.webp";
+import localizationCriciuma from "../assets/franchises/criciuma/localization.webp";
+import whatsappForquilhinha from "../assets/franchises/forquilhinha/whatsapp.webp";
+import localizationForquilhinha from "../assets/franchises/forquilhinha/localization.webp";
+
+export const franchisesPages = [
+  {
+    route: "/biguacu",
+    whatsapp: whatsappBiguacu,
+    localization: localizationBiguacu,
+    whatsappNumber: "48991920452",
+    mapAddress:
+      "https://www.google.com/maps/place/R.+C%C3%B4nego+Rodolfo+Machado,+1000+-+Rio+Caveiras,+Bigua%C3%A7u+-+SC,+88161-732/@-27.5064736,-48.6670961,14.58z/data=!4m6!3m5!1s0x95274c3bc4256687:0xd8aa1ff4adcd8118!8m2!3d-27.5055183!4d-48.6530734!16s%2Fg%2F11gdg5j99v?entry=ttu",
+  },
+  {
+    route: "/meleiro",
+    whatsapp: whatsappMeleiro,
+    localization: localizationMeleiro,
+    whatsappNumber: "48991981883",
+    mapAddress:
+      "https://www.google.com/maps/search/Rua+Vicinal+Dos+Imigrantes,+51,+Zanette+-+Meleiro/@-28.8383595,-49.6266355,17.78z?entry=ttu&g_ep=EgoyMDI1MDIyMy4xIKXMDSoASAFQAw%3D%3D",
+  },
+  {
+    route: "/porto-uniao",
+    whatsapp: whatsappPortoUniao,
+    localization: localizationPortoUniao,
+    whatsappNumber: "42991442738",
+    mapAddress:
+      "https://www.google.com/maps?sca_esv=5d466d0b992978df&output=search&q=AV.+EXPEDICIONARIO+EDMUNDO+ARRABAR,+1733+-+SANTA+ROSA+-+PORTO+UNI%C3%83O+-+SC&source=lnms&fbs=ABzOT_BYhiZpMrUAF0c9tORwPGlssxTeO7BMOlalbdyQXag72r1qnNd-MghSp-MLF7ZupKLNJpA1I8VZ0HfUeA-4UgdXtgv7KrhqJbSrd3iVRkvoRefjirRX85Ua6jyKigAgdGNZLoTsXF1dgDzkTCPA3WycVLPKGYG1JdVrCz0AvP3iYc3SfqU5KeIbbHtuwymECMvVnTxVsIiBeGEaQRwplqnuL5N-5A&entry=mc&ved=1t:200715&ictx=111",
+  },
+  {
+    route: "/icara",
+    whatsapp: whatsappIcara,
+    localization: localizationIcara,
+    whatsappNumber: "4834322019",
+    mapAddress:
+      "https://www.google.com/maps?sca_esv=5d466d0b992978df&output=search&q=RUA+LEOBERTO+LEAL,+390+-+CENTRO+-+I%C3%87ARA+-+SC&source=lnms&fbs=ABzOT_BYhiZpMrUAF0c9tORwPGlssxTeO7BMOlalbdyQXag72r1qnNd-MghSp-MLF7ZupKLNJpA1I8VZ0HfUeA-4UgdXtgv7KrhqJbSrd3iVRkvoRefjirRX85Ua6jyKigAgdGNZLoTsXF1dgDzkTCPA3WycVLPKGYG1JdVrCz0AvP3iYc3SfqU5KeIbbHtuwymECMvVnTxVsIiBeGEaQRwplqnuL5N-5A&entry=mc&ved=1t:200715&ictx=111",
+  },
+  {
+    route: "/criciuma-centro",
+    whatsapp: whatsappCriciuma,
+    localization: localizationCriciuma,
+    whatsappNumber: "4834333107",
+    mapAddress:
+      "https://www.google.com/maps/place/R.+Cel.+Marcos+Rovaris,+755+-+Centro,+Crici%C3%BAma+-+SC,+88801-100/@-28.6784133,-49.3609924,17z/data=!3m1!4b1!4m6!3m5!1s0x95217880b8449b49:0x10ddd29e46fcfd!8m2!3d-28.6784133!4d-49.3609924!16s%2Fg%2F11c5dfqlcv?entry=ttu&g_ep=EgoyMDI0MDgyMy4wIKXMDSoASAFQAw%3D%3D",
+  },
+  {
+    route: "/forquilhinha",
+    whatsapp: whatsappForquilhinha,
+    localization: localizationForquilhinha,
+    whatsappNumber: "48991436778",
+    mapAddress:
+      "https://www.google.com/maps/place/R.+Nereu+Belolli,+35+-+Ouro+Negro,+Forquilhinha+-+SC,+88850-000/@-28.7499724,-49.4490255,17z/data=!3m1!4b1!4m6!3m5!1s0x952181548a31358f:0x55aea3fe153d2601!8m2!3d-28.7499772!4d-49.44416!16s%2Fg%2F11y6hhwcgh?entry=ttu&g_ep=EgoyMDI1MDQzMC4xIKXMDSoASAFQAw%3D%3D",
+  },
+];
