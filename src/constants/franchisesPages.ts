1c7c8ac40eff5ab6c4daae7de6f9ef369367f6ed
Lucas Sagas <lucas.sagas@rdstation.com>
2025-06-24 13:24:27 -0300

    add Rio Maina franchise details and update related routes

--- src/constants/franchisesPages.ts
+++ src/constants/franchisesPages.ts
@@ -10,6 +10,8 @@ import whatsappCriciuma from "../assets/franchises/criciuma/whatsapp.webp";
 import localizationCriciuma from "../assets/franchises/criciuma/localization.webp";
 import whatsappForquilhinha from "../assets/franchises/forquilhinha/whatsapp.webp";
 import localizationForquilhinha from "../assets/franchises/forquilhinha/localization.webp";
+import whatsappRioMaina from "../assets/franchises/riomaina/whatsapp.webp";
+import localizationRioMaina from "../assets/franchises/riomaina/localization.webp";
 
 export const franchisesPages = [
   {
@@ -59,5 +61,45 @@ export const franchisesPages = [
     whatsappNumber: "48991436778",
     mapAddress:
       "https://www.google.com/maps/place/R.+Nereu+Belolli,+35+-+Ouro+Negro,+Forquilhinha+-+SC,+88850-000/@-28.7499724,-49.4490255,17z/data=!3m1!4b1!4m6!3m5!1s0x952181548a31358f:0x55aea3fe153d2601!8m2!3d-28.7499772!4d-49.44416!16s%2Fg%2F11y6hhwcgh?entry=ttu&g_ep=EgoyMDI1MDQzMC4xIKXMDSoASAFQAw%3D%3D",
+  },
+  {
+    route: "/riomaina",
+    whatsapp: whatsappRioMaina,
+    localization: localizationRioMaina,
+    whatsappNumber: "5548991178955",
+    mapAddress:
+      "https://www.google.com/maps?sca_esv=da71dabda52a3bb7&output=search&q=RUA+LOURENCO+ZANETTE,+2111+-+SALA+02+-+VILA+FLORESTA+II+-+CRICI%C3%9AMA+-+SC&source=lnms&fbs=ABzOT_BYhiZpMrUAF0c9tORwPGlssxTeO7BMOlalbdyQXag72t9gqHh-513MhadK60-5S3Q1AUveOKhRET8frzX92ux-vnXnkpqzO1Kej_z9IwTU4LRN1MaNtoZacaKs6stl55SKIJ6T5lP7qlPHoUSulqC-nUDOcBOGYD73JSYd-bS9Nc8dsV233JkQeokpMDekA2xYg6LnaUdHNlg_8TEsBBMDxJJzrA&entry=mc&ved=1t:200715&ictx=111",
+  },
+  {
+    route: "/arroiodosilva",
+    whatsapp: whatsappRioMaina,
+    localization: localizationRioMaina,
+    whatsappNumber: "5548991363461",
+    mapAddress:
+      "https://www.google.com/maps/place/ARROIO+DO+SILVA+VISTORIA+VEICULAR+LTDA/@-28.9824593,-49.423285,17z/data=!3m1!4b1!4m6!3m5!1s0x952233404492fc41:0x15faa6c93799dd16!8m2!3d-28.9824593!4d-49.4207101!16s%2Fg%2F11tcv6msjv?entry=ttu&g_ep=EgoyMDI1MDIyMy4xIKXMDSoASAFQAw%3D%3D",
+  },
+  {
+    route: "/pomerode",
+    whatsapp: whatsappRioMaina,
+    localization: localizationRioMaina,
+    whatsappNumber: "554733871883",
+    mapAddress:
+      "https://www.google.com/maps/place/R.+Hermann+Weege,+1474+-+Centro,+Pomerode+-+SC,+89107-000/@-26.725803,-49.1732076,17z/data=!3m1!4b1!4m6!3m5!1s0x94dee50d189f8d19:0xd7d3c954647eba3f!8m2!3d-26.7258078!4d-49.1706273!16s%2Fg%2F11f53pn_rs?entry=ttu&g_ep=EgoyMDI1MDIyMy4xIKXMDSoASAFQAw%3D%3D",
+  },
+  {
+    route: "/ilhota",
+    whatsapp: whatsappRioMaina,
+    localization: localizationRioMaina,
+    whatsappNumber: "5547992565690",
+    mapAddress:
+      "https://www.google.com/maps/place/R.+Izidoro+Maes,+115,+Ilhota+-+SC,+88320-000/@-26.9022203,-48.8283115,16.85z/data=!4m6!3m5!1s0x94df31dbd8842667:0xab4a1aa15280764!8m2!3d-26.9006539!4d-48.8263997!16s%2Fg%2F11c28zs7bh?entry=ttu",
+  },
+  {
+    route: "/cocaldosul",
+    whatsapp: whatsappRioMaina,
+    localization: localizationRioMaina,
+    whatsappNumber: "5548991763476",
+    mapAddress:
+      "https://www.google.com/maps/place/R.+S%C3%A3o+Marcos,+813+-+b+-+Cristo+Rei,+Cocal+do+Sul+-+SC,+88845-000/@-28.5913825,-49.3248125,17z/data=!3m1!4b1!4m5!3m4!1s0x95217707277827b3:0x5f7687fe69e770e5!8m2!3d-28.5913872!4d-49.3222376?entry=ttu",
   },
 ];
