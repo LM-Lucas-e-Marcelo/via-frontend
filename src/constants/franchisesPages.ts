b16a8e804fca0846b3ae6feec61d626aeb717245
Lucas Sagas <lucas.sagas@rdstation.com>
2025-07-01 12:48:10 -0300

    add precautionary flag for franchise locations

--- src/constants/franchisesPages.ts
+++ src/constants/franchisesPages.ts
@@ -27,12 +27,14 @@ export const franchisesPages = [
     whatsappNumber: "48991920452",
     mapAddress:
       "https://www.google.com/maps/place/VIA+VISTORIA+VEICULAR+BIGUA%C3%87U/@-27.5054199,-48.655416,17z/data=!3m1!4b1!4m6!3m5!1s0x95274ded8a17d7a7:0xc148a7f7e4388a21!8m2!3d-27.5054247!4d-48.6528411!16s%2Fg%2F11hf6rmdkx?entry=ttu&g_ep=EgoyMDI1MDYyMi4wIKXMDSoASAFQAw%3D%3D",
+    hasPrecautionary: true,
   },
   {
     route: "/meleiro",
     whatsapp: whatsappMeleiro,
     localization: localizationMeleiro,
     whatsappNumber: "48991981883",
+    hasPrecautionary: true,
     mapAddress:
       "https://www.google.com/maps/place/VIA+VISTORIA+MELEIRO+-+VISTORIA+VEICULAR/@-28.8378742,-49.6303329,17z/data=!4m10!1m2!2m1!1svia+vistoria+Rua+Vicinal+Dos+Imigrantes,+51,+Zanette+-+Meleiro+-+SC+-+No+Trevo+De+Meleiro!3m6!1s0x95218b1354af4451:0xf7da874b775df9fe!8m2!3d-28.8378789!4d-49.627758!15sCll2aWEgdmlzdG9yaWEgUnVhIFZpY2luYWwgRG9zIEltaWdyYW50ZXMsIDUxLCBaYW5ldHRlIC0gTWVsZWlybyAtIFNDIC0gTm8gVHJldm8gRGUgTWVsZWlyb5IBEnZlaGljbGVfaW5zcGVjdGlvbuABAA!16s%2Fg%2F11wx4v4_wq?entry=ttu&g_ep=EgoyMDI1MDYxNy4wIKXMDSoASAFQAw%3D%3D",
   },
@@ -43,6 +45,7 @@ export const franchisesPages = [
     whatsappNumber: "5542991442738",
     mapAddress:
       "https://www.google.com/maps/place/VIA+PORTO+UNI%C3%83O+-+VISTORIA+VEICULAR+E+LAUDO+CAUTELAR/@-26.2531853,-51.0723227,17z/data=!3m1!4b1!4m6!3m5!1s0x94e68b273c399cfb:0xa87aedb73afe21fd!8m2!3d-26.2531901!4d-51.0697478!16s%2Fg%2F11rqs6ct4b?entry=ttu&g_ep=EgoyMDI1MDYyMi4wIKXMDSoASAFQAw%3D%3D",
+    hasPrecautionary: true,
   },
   {
     route: "/icara",
@@ -51,6 +54,7 @@ export const franchisesPages = [
     whatsappNumber: "4834322019",
     mapAddress:
       "https://www.google.com/maps/place/VIA+I%C3%87ARA+-+VISTORIA+VEICULAR+E+LAUDO+CAUTELAR/@-28.7079921,-49.3013941,17z/data=!3m1!4b1!4m6!3m5!1s0x95217ec84ae848df:0x6711118c817493e7!8m2!3d-28.7079968!4d-49.2988192!16s%2Fg%2F11cr_qpx8r?entry=ttu&g_ep=EgoyMDI1MDYyMi4wIKXMDSoASAFQAw%3D%3D",
+    hasPrecautionary: true,
   },
   {
     route: "/criciuma",
@@ -59,6 +63,7 @@ export const franchisesPages = [
     whatsappNumber: "4834333107",
     mapAddress:
       "https://www.google.com/maps/place/VIA+CRICI%C3%9AMA+-+VISTORIA+VEICULAR+E+LAUDO+CAUTELAR/@-28.6785,-49.3633492,17z/data=!3m1!4b1!4m6!3m5!1s0x9521788f630feccb:0xad54e5b60f0b6a05!8m2!3d-28.6785047!4d-49.3607743!16s%2Fg%2F11bw21j0sl?entry=ttu&g_ep=EgoyMDI1MDYyMi4wIKXMDSoASAFQAw%3D%3D",
+    hasPrecautionary: true,
   },
   {
     route: "/forquilhinha",
@@ -67,6 +72,7 @@ export const franchisesPages = [
     whatsappNumber: "48991436778",
     mapAddress:
       "https://www.google.com/maps/place/VIA+VISTORIA+VEICULAR+-+FORQUILHINHA/@-28.7499772,-49.44416,17z/data=!4m15!1m8!3m7!1s0x952181548a31358f:0x55aea3fe153d2601!2sR.+Nereu+Belolli,+35+-+Ouro+Negro,+Forquilhinha+-+SC,+88850-000!3b1!8m2!3d-28.7499772!4d-49.44416!16s%2Fg%2F11y6hhwcgh!3m5!1s0x952185b2e254f53d:0x21cc7bf075c6a3aa!8m2!3d-28.7499772!4d-49.44416!16s%2Fg%2F11rp0ywdh8?entry=ttu&g_ep=EgoyMDI1MDYyMi4wIKXMDSoASAFQAw%3D%3D",
+    hasPrecautionary: true,
   },
   {
     route: "/riomaina",
@@ -75,6 +81,7 @@ export const franchisesPages = [
     whatsappNumber: "5548991178955",
     mapAddress:
       "https://www.google.com/maps/place/VIA+RIO+MAINA+-+VISTORIA+VEICULAR+E+LAUDO+CAUTELAR/@-28.6752638,-49.4110622,14z/data=!4m10!1m2!2m1!1svia+vistoria+R.+Louren%C3%A7o+Zanette,+2111+-+02+-+Vila+Zuleima!3m6!1s0x95218302ab5d348b:0xbe185e0bdefe8f58!8m2!3d-28.6760342!4d-49.4076182!15sCjt2aWEgdmlzdG9yaWEgUi4gTG91cmVuw6dvIFphbmV0dGUsIDIxMTEgLSAwMiAtIFZpbGEgWnVsZWltYVo3IjV2aWEgdmlzdG9yaWEgciBsb3VyZW7Dp28gemFuZXR0ZSAyMTExIDAyIHZpbGEgenVsZWltYZIBFmNhcl9pbnNwZWN0aW9uX3N0YXRpb26aASRDaGREU1VoTk1HOW5TMFZKUTBGblNVUjJaMjlVY1RWUlJSQUKqAW8QASoQIgx2aWEgdmlzdG9yaWEoDDIeEAEiGkk2NAEUwSjgl1ROlHAVb0FZVcaaBCZwQBLCMjkQAiI1dmlhIHZpc3RvcmlhIHIgbG91cmVuw6dvIHphbmV0dGUgMjExMSAwMiB2aWxhIHp1bGVpbWHgAQD6AQQIABBF!16s%2Fg%2F11j3444ndp?entry=ttu&g_ep=EgoyMDI1MDYyMi4wIKXMDSoASAFQAw%3D%3D",
+    hasPrecautionary: true,
   },
   {
     route: "/arroiodosilva",
@@ -83,6 +90,7 @@ export const franchisesPages = [
     whatsappNumber: "5548991363461",
     mapAddress:
       "https://www.google.com/maps/place/ARROIO+DO+SILVA+VISTORIA+VEICULAR+LTDA/@-28.9824593,-49.423285,17z/data=!3m1!4b1!4m6!3m5!1s0x952233404492fc41:0x15faa6c93799dd16!8m2!3d-28.9824593!4d-49.4207101!16s%2Fg%2F11tcv6msjv?entry=ttu&g_ep=EgoyMDI1MDIyMy4xIKXMDSoASAFQAw%3D%3D",
+    hasPrecautionary: false,
   },
   {
     route: "/pomerode",
@@ -91,6 +99,7 @@ export const franchisesPages = [
     whatsappNumber: "554733871883",
     mapAddress:
       "https://www.google.com/maps/place/VIA+VISTORIA+VEICULAR+-+POMERODE/@-26.7257623,-49.1731183,17z/data=!3m1!4b1!4m6!3m5!1s0x94dee50600cee8bb:0xfd18115318de2ffc!8m2!3d-26.7257671!4d-49.1705434!16s%2Fg%2F11hdq9y0y_?entry=ttu&g_ep=EgoyMDI1MDYyMi4wIKXMDSoASAFQAw%3D%3D",
+    hasPrecautionary: true,
   },
   {
     route: "/ilhota",
@@ -99,6 +108,7 @@ export const franchisesPages = [
     whatsappNumber: "5547992565690",
     mapAddress:
       "https://www.google.com/maps/place/VIA+ILHOTA+-+VISTORIA+VEICULAR/@-26.9007369,-48.8290981,17z/data=!3m1!4b1!4m6!3m5!1s0x94df3118d1c34051:0xc0f78656bdf5fb90!8m2!3d-26.9007417!4d-48.8265232!16s%2Fg%2F11lmhpng9r?entry=ttu&g_ep=EgoyMDI1MDYyMi4wIKXMDSoASAFQAw%3D%3D",
+    hasPrecautionary: false,
   },
   {
     route: "/cocaldosul",
@@ -107,5 +117,6 @@ export const franchisesPages = [
     whatsappNumber: "5548991763476",
     mapAddress:
       "https://www.google.com/maps/place/VIA+Vistoria+Veicular/@-28.5913825,-49.3248125,17z/data=!3m1!4b1!4m6!3m5!1s0x9521779b4c3d8fb9:0x138b1f93e0ed6c46!8m2!3d-28.5913872!4d-49.3222376!16s%2Fg%2F11rgzvz75f?entry=ttu&g_ep=EgoyMDI1MDYyMi4wIKXMDSoASAFQAw%3D%3D",
+    hasPrecautionary: true,
   },
 ];
