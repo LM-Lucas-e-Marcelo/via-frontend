9c92d771ae6ecfde2b98cc49dc838409ea8c5fb4
Lucas Sagás <lucassaagas@gmail.com>
2026-02-25 10:30:10 -0300

    add new franchises

--- src/constants/addresses.ts
+++ src/constants/addresses.ts
@@ -9,6 +9,10 @@ import arroio from "../assets/arroio.png";
 import icara from "../assets/icara.png";
 import porto from "../assets/porto_uniao.png";
 import forquilhinha from "../assets/forquilhinha.png";
+import icaraLiri from "../assets/icara_liri.png";
+import santaRosaDoSul from "../assets/via_santa_rosa_do_sul.png";
+import lages from "../assets/via_lages.png";
+import gravatal from "../assets/via_gravatal.png";
 
 export const addresses = [
   {
@@ -76,6 +80,15 @@ export const addresses = [
     image: forquilhinha,
   },
   {
+    id: "gravatal",
+    cityAndNeigh: "VIA VISTORIA GRAVATAL",
+    street: "Rua Manoel João Moraes Coop, 42 - Gravatal - SC",
+    phoneNumber: "(48) 99206-1021",
+    whatsappNumber: "48992061021",
+    mapUrl: "https://www.google.com/maps?client=firefox-b-d&hs=muPp&sca_esv=ee81a51e8e25352b&output=search&q=VIA+VISTORIA+GRAVATAL++Rua+Manoel+Jo%C3%A3o+Moraes+Coop,+42+,+Gravatal,+SC&source=lnms&fbs=ADc_l-acAb_3MMOAUx0zmbUpgBqRNDksEaVrhivIH8sZJpxqoTq330TkReHURF1S6FtwdammcHPDe6If4FraTj_toiqY-xHXveAh4UJjqD_uJ2JX2moRD-n82wdkbNrajL5mdkwhNlKtZpwUc2umLj-KRs77W1_bOefe6PXy9s0-96CTvyIOo9BCMIvG47QRFBNiCgpXnhVOYmay99qB8QagmbvvMly9tg&entry=mc&ved=1t:200715&ictx=111",
+    image: gravatal,
+  },
+  {
     id: "icara",
     cityAndNeigh: "VIA VISTORIA IÇARA",
     street: "Rua Leoberto Leal, 390 - Centro - Içara - SC",
@@ -86,6 +99,16 @@ export const addresses = [
     image: icara,
   },
   {
+    id: "icara_liri",
+    cityAndNeigh: "VIA VISTORIA IÇARA LIRI",
+    street: "ROD SC 445, 3100 SALA 11,12 e 13 - Liri - Içara - SC",
+    phoneNumber: "(48) 99144-4935",
+    whatsappNumber: "48991444935",
+    mapUrl:
+      "https://www.google.com/maps/place/SC-445,+3100+-+Liri,+I%C3%A7ara+-+SC,+88820-000/@-28.6973588,-49.3171333,17z/data=!4m6!3m5!1s0x9521793282a145d9:0x48ff669ae9f2e1e9!8m2!3d-28.6973588!4d-49.314553!16s%2Fg%2F11jyy4n58j?entry=ttu&g_ep=EgoyMDI2MDIyMi4wIKXMDSoASAFQAw%3D%3D",
+    image: icaraLiri,
+  },
+  {
     id: "ilhota",
     cityAndNeigh: "VIA VISTORIA ILHOTA",
     street: "R. Izidoro Maes, 115, Centro, Ilhota - SC",
@@ -94,6 +117,16 @@ export const addresses = [
     mapUrl:
       "https://www.google.com/maps/place/VIA+ILHOTA+-+VISTORIA+VEICULAR/@-26.9007369,-48.8290981,17z/data=!3m1!4b1!4m6!3m5!1s0x94df3118d1c34051:0xc0f78656bdf5fb90!8m2!3d-26.9007417!4d-48.8265232!16s%2Fg%2F11lmhpng9r?entry=ttu&g_ep=EgoyMDI1MDYyMi4wIKXMDSoASAFQAw%3D%3D",
     image: ilhota,
+  },
+  {
+    id: "lages",
+    cityAndNeigh: "VIA VISTORIA LAGES",
+    street: "Av. Dom Pedro II, 2288 - Centro - Lages - SC",
+    phoneNumber: "(49) 3019-0018",
+    whatsappNumber: "554930190018",
+    mapUrl:
+      "https://www.google.com/maps/place/VIA+VISTORIA+VEICULAR+-+LAGES/@-27.8244138,-50.3267171,17z/data=!3m1!4b1!4m6!3m5!1s0x94e01fcf1a8d88b7:0x141d67897981cc80!8m2!3d-27.8244138!4d-50.3241368!16s%2Fg%2F11fnrdbwbm?entry=ttu&g_ep=EgoyMDI2MDIyMi4wIKXMDSoASAFQAw%3D%3D",
+    image: lages,
   },
   {
     id: "meleiro",
@@ -126,5 +159,15 @@ export const addresses = [
     mapUrl:
       "https://www.google.com/maps/place/VIA+PORTO+UNI%C3%83O+-+VISTORIA+VEICULAR+E+LAUDO+CAUTELAR/@-26.2531853,-51.0723227,17z/data=!3m1!4b1!4m6!3m5!1s0x94e68b273c399cfb:0xa87aedb73afe21fd!8m2!3d-26.2531901!4d-51.0697478!16s%2Fg%2F11rqs6ct4b?entry=ttu&g_ep=EgoyMDI1MDYyMi4wIKXMDSoASAFQAw%3D%3D",
     image: porto,
+  },
+  {
+    id: "santa_rosa_do_sul",
+    cityAndNeigh: "VIA VISTORIA SANTA ROSA DO SUL",
+    street: "R. Raul José dos Santos, 51 - sala 03 - Centro - Santa Rosa do Sul - SC",
+    phoneNumber: "(48) 99161-0799",
+    whatsappNumber: "48991610799",
+    mapUrl:
+      "https://www.google.com/maps/place/VIA+VISTORIA+-+SANTA+ROSA+DO+SUL/@-29.1347427,-49.717151,17z/data=!3m1!4b1!4m6!3m5!1s0x9522176d0c71eb91:0xb76f955b2b130c7d!8m2!3d-29.1347427!4d-49.7145707!16s%2Fg%2F11s_v9rth5?entry=ttu&g_ep=EgoyMDI2MDIyMi4wIKXMDSoASAFQAw%3D%3D",
+    image: santaRosaDoSul,
   },
 ];
